6367169c761284b18db3577931dbee2fc00280ba
Bastien Goulet <bastien.goulet@uqar.ca>
2023-04-30 21:15:54 -0400

    Implemented Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule], // Make sure the AppRouting is last in the array
+  imports: [BrowserModule, AppRoutingModule], // Make sure the AppRouting is last in the array
   providers: [],
   bootstrap: [AppComponent],
 })
