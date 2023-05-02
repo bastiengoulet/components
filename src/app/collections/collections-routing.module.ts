9af37b88a0177e9966b30ba2e8a4c554ca797ec3
Bastien Goulet <bastien.goulet@uqar.ca>
2023-05-02 11:26:13 -0400

    Created Tabs component

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
-const routes: Routes = [{ path: '', component: CollectionsHomeComponent }];
+const routes: Routes = [
+  {
+    path: '', // Route: collections/
+    component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent }, // Route: collections/
+      { path: 'companies', component: CompaniesComponent }, // Route: collections/companies
+      { path: 'partners', component: PartnersComponent }, // Route: collections/partners
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
