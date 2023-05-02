0fd9155a33f6f961e6ae9aa8b826289f7977cea3
Bastien Goulet <bastien.goulet@uqar.ca>
2023-05-02 11:41:04 -0400

    Created Statistics Component

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Statistics } from '../interfaces/statistics';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css'],
+})
+export class ViewsHomeComponent {
+  stats: Statistics[] = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
+}
