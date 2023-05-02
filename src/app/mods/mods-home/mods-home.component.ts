1949da16e687647cf7d3c51d71477488c6f516d2
Bastien Goulet <bastien.goulet@uqar.ca>
2023-05-02 12:29:50 -0400

    Created a reusable Modal component

---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mods-home',
+  templateUrl: './mods-home.component.html',
+  styleUrls: ['./mods-home.component.css'],
+})
+export class ModsHomeComponent {
+  modalOpen: boolean = false;
+
+  onClick() {
+    this.modalOpen = !this.modalOpen;
+  }
+}
